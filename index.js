55af7b91ca92e661ed7b00483bb1bf52bd3d0c07
Rinor Rama <rinor.rama95@gmail.com>
2021-11-22 15:08:00 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+function askForStartingLetter() {
+	return prompt("Choose a starting letter: ");
+}
+
+function doesUserNameContainLetter(user) {
+	return user.name.indexOf(startingLetter) > -1;
+}
+function getUserName(user) {
+	return user.name;
+}
+function greet(username) {
+	console.log(`Hello ${username}`);
+}
+function askForUserId() {
+	return Number(prompt("Enter your user ID"));
+}
+function showIncompleteTodos(userId) {
+	const incompleteTodos = window.todos.filter(function (todo) {
+		return todo.userId === userId && !todo.completed;
+	});
+	const incompleteTodoTitles = incompleteTodos.map(function (todo) {
+		return todo.title;
+	});
+	console.log(`These are your incomplete tasks: `);
+	for (const todoTitle of incompleteTodoTitles) {
+		console.log(todoTitle);
+	}
+}
+function askForProperty() {
+	return prompt("Enter a property :");
+}
+
+function showUserProperties(property) {
+	for (const user of window.users) {
+		console.log(`${user.name} - `, user[property]);
+	}
+}
+
+const startingLetter = askForStartingLetter();
+const selectedUsers = window.users.filter(doesUserNameContainLetter);
+const selectedUserNames = selectedUsers.map(getUserName);
+
+let i = 0;
+let intervalId = setInterval(function () {
+	if (i === selectedUserNames.length) {
+		clearInterval(intervalId);
+		const selectedUserId = askForUserId();
+		showIncompleteTodos(selectedUserId);
+		const property = askForProperty();
+		showUserProperties(property);
+	} else {
+		greet(selectedUserNames[i]);
+		i++;
+	}
+}, 2000);
